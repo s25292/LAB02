f061cb219684e96b2acedbdfc8f074573f9bce43
s25292 <s25292@pjwstk.edu.pl>
2024-03-11 16:34:33 +0100

    Modyfikacja 3

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Modyfikacja 1");
         Console.WriteLine("Modyfikacja 2");
+        Console.WriteLine("Modyfikacja 3");
     }
 }
